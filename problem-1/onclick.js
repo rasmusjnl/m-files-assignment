d04e8a1893470a04d3b727494359cbc183cc6ed4
Rasmus Personal <personal@FI-R2GP93FVPD.local>
2024-11-04 20:54:32 +0200

    Assignment 1 done

---
+++ problem-1/onclick.js
@@ -0,0 +1,15 @@
+function suggestionElementPreparation(suggestionElemClass, suggestionClickEvent) {
+    const suggestionElem = document.getElementById("mf-suggestion-event");
+
+    if (suggestionElem) {
+        // Do something with suggestionElemClass here?
+        suggestionElem.onclick = suggestionClickEvent
+    }
+}
+
+// Pass the function parameter as an arrow function to prevent it from firing immediately when calling "suggestionElementPreparation"
+suggestionElementPreparation(
+    "mf-web-suggestionicon-text-mobile", 
+    () => webURLPreparation(true, false)
+)
+
